b63ff4d79e8038fbcf9e303959e740e5417f449e
mpani03 <milkmilkxciii@gmail.com>
2024-10-27 20:40:27 +0700

    Fix flag and sport table in SportDetail

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,14 @@ module.exports = {
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      scale: {
+        '175' : '1.75',
+        '200': '2',   
+        '250': '2.5',
+        '300': '3',
+      }
+    },
     fontFamily: {
       sans: ['Montserrat', 'sans-serif'],
       cinzel: ['Cinzel', 'serif'],
